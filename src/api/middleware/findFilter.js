49e99fe984c33ee31d4f492b7ccf2b7e9435cf0b
lucas-hirata <lucashirata.dev@gmail.com>
2021-04-10 16:25:04 -0300

    implementada consulta GET com paginação

---
+++ src/api/middleware/findFilter.js
@@ -0,0 +1,76 @@
+class FindFilter {
+    filterByQuery = (model, populate) => async (request, response, next) => {
+        let requestQuery = { ...request.query };
+
+        const paramsToRemove = ['select', 'sort', 'page', 'limit'];
+
+        paramsToRemove.forEach((param) => delete requestQuery[param]);
+
+        let queryString = JSON.stringify(requestQuery);
+        queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
+
+        // Finding resources
+        let query = model.find(JSON.parse(queryString));
+
+        // Selecting
+        if (request.query.select) {
+            const fields = request.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        // Sorting
+        if (request.query.sort) {
+            const sortBy = request.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        // Pagination
+        const page = parseInt(request.query.page, 10) || 1;
+        const limit = parseInt(request.query.limit, 10);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const total = await model.countDocuments();
+
+        if (request.query.limit) {
+            query = query.skip(startIndex).limit(limit);
+        }
+
+        if (populate) {
+            query = query.populate(populate);
+        }
+
+        // Query execution
+        const results = await query;
+
+        // Pagination result
+        let pagination = null;
+
+        if (request.query.limit) {
+            pagination = {};
+
+            pagination.current = page;
+
+            if (endIndex < total) {
+                pagination.next = page + 1;
+            }
+
+            if (startIndex > 0) {
+                pagination.previous = page - 1;
+            }
+
+            pagination.limit = limit;
+            pagination.total = total;
+        }
+
+        response.filterResults = {
+            data: results,
+            pagination,
+        };
+
+        next();
+    };
+}
+
+module.exports = new FindFilter();
