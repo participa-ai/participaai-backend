a45bc5ab93ce985d9c107926458c6a57c70cfa1b
lucas-hirata <lucashirata.dev@gmail.com>
2021-04-10 16:24:12 -0300

    corrigida parada do servidor em exceção não tratada

--- server.js
+++ server.js
@@ -21,4 +21,5 @@ process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red);
     // Close server & exit process
     server.close(() => process.exit(1));
+    throw err;
 });
