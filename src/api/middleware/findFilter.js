5ef0ce467b2a483a0f180026b33cb08cd66ef185
lucas-hirata <lucashirata.dev@gmail.com>
2021-04-10 20:07:55 -0300

    removida opçao de listar registros deletados e corrigido contagem de busca com paginacao

--- src/api/middleware/findFilter.js
+++ src/api/middleware/findFilter.js
@@ -5,13 +5,7 @@ class FindFilter {
         const paramsToRemove = ['select', 'sort', 'page', 'limit', 'senha'];
         paramsToRemove.forEach((param) => delete requestQuery[param]);
 
-        if (!requestQuery.deleted) {
-            requestQuery.deleted = false;
-        } else {
-            if (requestQuery.deleted === 'any') {
-                delete requestQuery.deleted;
-            }
-        }
+        requestQuery.deleted = false;
 
         let queryString = JSON.stringify(requestQuery);
         queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
@@ -44,7 +38,7 @@ class FindFilter {
         const limit = parseInt(request.query.limit, 10);
         const startIndex = (page - 1) * limit;
         const endIndex = page * limit;
-        const total = await model.countDocuments();
+        const total = await model.countDocuments({ deleted: false });
 
         if (request.query.limit) {
             query = query.skip(startIndex).limit(limit);
