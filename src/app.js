06e9beeaa25c53e1b34ec37dd2b7733672de18ff
lucas-hirata <lucashirata.dev@gmail.com>
2021-05-08 15:01:07 -0300

    adicionado packages de protecao e limite de req

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
+
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+
 const errorHandler = require('./api/middleware/errorHandler');
 const loader = require('./utils/loaders/app');
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 300,
+});
 
 const app = express()
     .use(express.json())
     .use(cors())
     .use(cookieParser())
+    .use(mongoSanitize())
+    .use(helmet())
+    .use(xss())
+    .use(hpp())
+    .use(limiter)
     .use(express.static(path.join(__dirname, 'public')));
 
 loader(app);
